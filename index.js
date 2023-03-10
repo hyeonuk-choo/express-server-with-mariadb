2f86104fc81849278ca9dc84073ee1f91d5de1b6
hyeonuk-choo <hyeonuk97@naver.com>
2023-03-10 15:03:53 +0900

    feat: Add CRUD features

--- index.js
+++ index.js
@@ -6,11 +6,102 @@ const { userinfo } = require("./userInfo.js");
 const { week, month } = require("./rank.js");
 const { posts } = require("./data.js");
 const { dday } = require("./dday.js");
-const { todos } = require("./todos.js");
+const todosData = require("./todos.js");
 const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(cors());
 app.use(express.json());
+
+app.put("/api/todo-update", (req, res) => {
+  const updatedTodo = req.body; // get the updated todo item from the request body
+  try {
+    // Map over the todos array and replace the matching todo item with the updated todo item
+    // const updatedTodos = todosData.todos.map((todo) => {
+    //   if (todo.id === updatedTodo.id) {
+    //     return { ...todo, ...updatedTodo };
+    //   }
+    //   return todo;
+    // });
+
+    // Check if the todo item was found and updated
+    if (updatedTodo === todosData.todos) {
+      return res.status(404).send("Todo item not found.");
+    }
+
+    // Update the todos array with the new array
+    todosData.todos = updatedTodo.todos;
+
+    // Write updated todos array to file
+    const filePath = path.join(__dirname, "todos.js");
+    fs.writeFileSync(filePath, `module.exports = ${JSON.stringify(todosData)}`);
+
+    res.status(200).send("Todo item updated successfully.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating todo item.");
+  }
+});
+
+app.delete("/api/todo-delete", (req, res) => {
+  const { id } = req.body; // get the id from the request body
+
+  try {
+    // Remove the todo item with the specified id from the todos array
+    const filteredTodos = todosData.todos.filter((todo) => todo.id !== id);
+
+    // If the length of the todos array changed, a todo item was removed
+    if (filteredTodos.length < todosData.todos.length) {
+      todosData.todos = filteredTodos;
+
+      // Write updated todos array to file
+      const filePath = path.join(__dirname, "todos.js");
+      fs.writeFileSync(
+        filePath,
+        `module.exports = ${JSON.stringify(todosData)}`
+      );
+
+      res.setHeader("Cache-Control", "no-cache");
+      res.setHeader("Surrogate-Control", "no-store");
+      res.status(200).send(`Todo item with id ${id} was deleted successfully.`);
+    } else {
+      res.status(404).send(`Todo item with id ${id} not found.`);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting todo item.");
+  }
+});
+
+app.post("/api/todo-add", (req, res) => {
+  delete require.cache[require.resolve("./todos.js")];
+  const newTodo = req.body; // get the new data from the request body
+
+  try {
+    // newTodo.id = todosData.todos.length + 1 --> key값 중복에러 발생
+    // uuid library로 유일한 키값 생성하여 할당
+    // push 메서드 대신, unshift 메서드를 사용하여 최근추가 데이터를 상위로 생성
+    newTodo.id = uuidv4();
+    todosData.todos.unshift(newTodo);
+    fs.writeFileSync(
+      "./todos.js",
+      `module.exports = ${JSON.stringify(todosData)}`
+    );
+    res.status(200).send("your Requeest success^^");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating data.");
+  }
+});
+
+app.get("/api/planner-main", (req, res) => {
+  delete require.cache[require.resolve("./todos.js")];
+  let todos2 = require("./todos.js");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Surrogate-Control", "no-store");
+  res.json(todos2.todos);
+});
 
 app.get("/api/dday", (req, res) => {
   res.json(dday);
@@ -34,33 +125,6 @@ app.get("/api/rank/month", (req, res) => {
   if (page == 1) res.json(month[1]);
   if (page == 2) res.json(month[2]);
   if (page == 3) res.json(month[3]);
-});
-
-app.post("/api/todo-add", (req, res) => {
-  const newData = req.body; // get the new data from the request body
-  // write the updated data back to the todos module
-
-  fs.writeFileSync(
-    "./todos.js",
-    `module.exports = {todos:[${JSON.stringify(newData)}]}`,
-    (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error updating data.");
-      } else {
-        delete require.cache[require.resolve("./todos.js")];
-        res.status(200).send("your Requeest success^^");
-      }
-    }
-  );
-});
-
-app.get("/api/planner-main", (req, res) => {
-  delete require.cache[require.resolve("./todos.js")];
-  let todos2 = require("./todos.js");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Surrogate-Control", "no-store");
-  res.json(todos2.todos);
 });
 
 app.listen(PORT, () => {
