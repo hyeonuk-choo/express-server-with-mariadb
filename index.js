ab2290189afa3e87095c0ac0bc0d543935d0a92f
hyeonuk-choo <hyeonuk97@naver.com>
2023-03-27 17:02:14 +0900

    feat: CRUD구현 with mariaDB

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
 
 // 데이터파일
 const { userinfo } = require("./userInfo.js");
@@ -31,58 +30,35 @@ const pool = mariadb.createPool({
   database,
 });
 
-async function executeQuery(query) {
+async function executeQuery(query, params) {
   let conn;
   try {
     conn = await pool.getConnection();
-    const result = await conn.query(query);
+    const result = await conn.query(query, params);
     return result;
   } catch (err) {
     throw err;
   } finally {
-    if (conn) conn.end();
+    if (conn) conn.release();
   }
 }
 
-// executeQuery("SELECT * FROM userinfo")
-//   .then((result) => {
-//     // res.setHeader("Cache-Control", "no-cache");
-//     // res.setHeader("Surrogate-Control", "no-store");
-//     console.log(result);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+app.post("/api/todo-create", async (req, res) => {
+  const newTodo = req.body;
+  const oneTodo = newTodo[0];
 
-app.get("/api/planner-main", (req, res) => {
-  // delete require.cache[require.resolve("./todos.js")];
-  // 조회query문 함수
-  executeQuery("SELECT * FROM students")
-    .then((result) => {
-      // res.setHeader("Cache-Control", "no-cache");
-      // res.setHeader("Surrogate-Control", "no-store");
-      console.log(result);
-      res.json(result);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
-
-app.put("/api/todo-update", (req, res) => {
-  const updatedTodo = req.body; // get the updated todo item from the request body
   try {
-    if (updatedTodo === todosData.todos) {
-      return res.status(404).send("Todo item not found.");
-    }
-
-    // Update the todos array with the new array
-    todosData.todos = updatedTodo.todos;
-
-    // Write updated todos array to file
-    const filePath = path.join(__dirname, "todos.js");
-    fs.writeFileSync(filePath, `module.exports = ${JSON.stringify(todosData)}`);
-
+    const query = `INSERT INTO todos (id, addMode, updateMode, isCompleted, title, inputMessage) VALUES (?, ? , ?, ?, ?, ?)`;
+    const params = [
+      newTodo[0].id,
+      newTodo[0].addMode,
+      newTodo[0].updateMode,
+      newTodo[0].isCompleted,
+      newTodo[0].title,
+      newTodo[0].inputMessage,
+    ];
+    const result = await executeQuery(query, params);
+    console.log(result);
     res.status(200).send("Todo item updated successfully.");
   } catch (err) {
     console.error(err);
@@ -90,69 +66,69 @@ app.put("/api/todo-update", (req, res) => {
   }
 });
 
-app.delete("/api/todo-delete", (req, res) => {
-  const { id } = req.body; // get the id from the request body
-
-  try {
-    // Remove the todo item with the specified id from the todos array
-    const filteredTodos = todosData.todos.filter((todo) => todo.id !== id);
-
-    // If the length of the todos array changed, a todo item was removed
-    if (filteredTodos.length < todosData.todos.length) {
-      todosData.todos = filteredTodos;
-
-      // Write updated todos array to file
-      const filePath = path.join(__dirname, "todos.js");
-      fs.writeFileSync(
-        filePath,
-        `module.exports = ${JSON.stringify(todosData)}`
-      );
-
-      res.setHeader("Cache-Control", "no-cache");
-      res.setHeader("Surrogate-Control", "no-store");
-      res.status(200).send(`Todo item with id ${id} was deleted successfully.`);
-    } else {
-      res.status(404).send(`Todo item with id ${id} not found.`);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error deleting todo item.");
-  }
-});
-
-app.post("/api/todo-add", (req, res) => {
-  delete require.cache[require.resolve("./todos.js")];
-  const newTodo = req.body; // get the new data from the request body
-
-  try {
-    // newTodo.id = todosData.todos.length + 1 --> key값 중복에러 발생
-    // uuid library로 유일한 키값 생성하여 할당
-    // push 메서드 대신, unshift 메서드를 사용하여 최근추가 데이터를 상위로 생성
-    newTodo.id = uuidv4();
-    todosData.todos.unshift(newTodo);
-    fs.writeFileSync(
-      "./todos.js",
-      `module.exports = ${JSON.stringify(todosData)}`
-    );
-    res.status(200).send("your Requeest success^^");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error updating data.");
-  }
-});
-
-app.get("/api/userinfo", (req, res) => {
-  executeQuery("SELECT * FROM userinfo")
+app.get("/api/planner-main", (req, res) => {
+  // delete require.cache[require.resolve("./todos.js")];
+  // 조회query문 함수
+  executeQuery("SELECT * FROM todos")
     .then((result) => {
       res.setHeader("Cache-Control", "no-cache");
       res.setHeader("Surrogate-Control", "no-store");
-      console.log(result);
       res.json(result);
     })
-    .catch((err) => {
-      console.error(err);
-    });
+    .catch((err) => {});
+});
+
+app.get("/api/userinfo", (req, res) => {
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Surrogate-Control", "no-store");
+  executeQuery("SELECT * FROM userinfo")
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {});
   // res.json(userinfo);
+});
+
+app.put("/api/todo-iscompleted", async (req, res) => {
+  try {
+    const updatedTodo = req.body;
+    console.log("updatedTodo", updatedTodo);
+
+    const query = `UPDATE todos SET isCompleted = NOT isCompleted WHERE id = '${updatedTodo[0].id}'`;
+
+    await executeQuery(query);
+    const getResult = await executeQuery("SELECT * FROM todos");
+    res.json(getResult);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating todo item.");
+  }
+});
+
+app.put("/api/todo-update", async (req, res) => {
+  try {
+    const updatedTodo = req.body;
+    console.log("updatedTodo", updatedTodo);
+    const query = `UPDATE todos SET title = '${updatedTodo[0].title}' WHERE id = '${updatedTodo[0].id}'`;
+    await executeQuery(query);
+    const getResult = await executeQuery("SELECT * FROM todos");
+    res.json(getResult);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating todo item.");
+  }
+});
+
+app.delete("/api/todo-delete", async (req, res) => {
+  try {
+    const payload = req.body; // get the id from the request body
+    console.log("payload", payload);
+
+    const query = `DELETE FROM todos WHERE id = '${payload.id}'`;
+    await executeQuery(query);
+    const getResult = await executeQuery("SELECT * FROM todos");
+    res.json(getResult);
+  } catch (error) {}
 });
 
 app.get("/api/rank/week", (req, res) => {
